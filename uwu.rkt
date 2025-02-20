cf7e58191d1f9fa89622977ea2960aba18d80053
esgore03 <143649079+esgore03@users.noreply.github.com>
2025-02-20 16:39:54 -0500

    Update uwu.rkt

--- uwu.rkt
+++ uwu.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
-;(define down(lambda (x)
-;(cond
-;[(empty? x) '()]
-;[else (cons (list(car x)) (down (cdr x)))]
-;)))
+(define down(lambda (x)
+(cond
+[(empty? x) '()]
+[else (cons (list(car x)) (down (cdr x)))]
+)))
 
 
 
@@ -15,6 +15,18 @@
 [else (cons (car l)(list-set (cdr l) (- n 1) x))]
 )))
 
-(define x '(5 9 8))
-(list-set x 2 "a")
 
+; EL APPEND NO ES MIO, USEN EL QUE CREARON .l.
+(define aux(lambda (n x memory)
+(let ( (node (car x))
+  )
+(cond
+[(equal? n node) memory]
+[(> n node) (aux n (caddr x) (append memory '("right")))]
+[else (aux n (cadr x) (append memory '("left") ))]
+))))
+
+
+(define myTree '(8 (3 (1 ()()) (6(4()())(7()()))) (10 () (14(13()())()))))
+
+(aux 13 myTree '())
